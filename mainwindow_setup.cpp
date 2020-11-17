bd07541215514f16f72927fc1aaf6df06ddeffb0
Stéphane <stephane@localhost.localdomain>
2020-11-17 17:22:12 +0100

    https://github.com/StephaneCouturier/Katalog/issues/114

--- mainwindow_setup.cpp
+++ mainwindow_setup.cpp
@@ -174,14 +174,16 @@
               "QTabBar               { background:  url(:images/Katalog40.png) no-repeat right; }"
               "QTabBar               { background-color: #0D79A6; qproperty-drawBase:0; outline: none; }"
               "QLabel                { color: #095676; }"
+              "QPushButton           { padding: 5px 4px; margin: 0px; } "
+              "QComboBox             { padding: 0px 0px; margin: 0px; } "
 
-              "QTabBar::tab          { background-color: #0D79A6 ; color: #F2F2F2; padding: 2px 12px; border: 1px solid #0D79A6; margin-bottom: 4px; margin-left: 4px;}"
+              "QTabBar::tab          { background-color: #0D79A6 ; color: #F2F2F2; padding: 2px; padding-left: 4px; padding-right: 20px; border: 1px solid #0D79A6; margin-bottom: 4px; margin-left: 4px;}"
               "QTabBar::tab:selected { background-color: #095676; color: #FFF; margin-top: 4px; margin-left: 4px; } "
               "QTabBar::tab:selected { border: 1px solid #095676; border-bottom: 0px; border-top-left-radius: 2px; border-top-right-radius: 2px; margin-bottom: 4px;} "
 
               "QTabBar::tab:!selected{ margin-top: 6px; }"
               "QTabWidget::tab-bar   { left: 0px; }"
-              "QTabWidget          { padding: 0px; margin: 0px; }"
+              "QTabWidget            { padding: 0px; margin: 0px; background-color: #095676; }"
 
 
               );
