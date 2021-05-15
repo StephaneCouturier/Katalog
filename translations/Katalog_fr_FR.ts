b250ad64af2829fcdab7190b0b0b9dc0ea969d3d
Stéphane <stephane@localhost.localdomain>
2021-05-15 12:51:30 +0200

    https://github.com/StephaneCouturier/Katalog/issues/137

--- translations/Katalog_fr_FR.ts
+++ translations/Katalog_fr_FR.ts
@@ -4,8 +4,316 @@
 <context>
     <name>MainWindow</name>
     <message>
+        <source>Catalog</source>
+        <translation>Catalogue</translation>
+    </message>
+    <message>
+        <source>Catalogs</source>
+        <translation>Catalogues</translation>
+    </message>
+    <message>
+        <source>Storage</source>
+        <translation>Stockage</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Lieu</translation>
+    </message>
+    <message>
+        <source>Explore</source>
+        <translation>Explorer</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>Créer</translation>
+    </message>
+    <message>
+        <source>Statistics</source>
+        <translation>Statistiques</translation>
+    </message>
+    <message>
         <source>Search</source>
         <translation>Chercher</translation>
+    </message>
+    <message>
+        <source>Total File Size</source>
+        <translation>Taille Totale des Fichiers</translation>
+    </message>
+    <message>
+        <source>Total Number of Files</source>
+        <translation>Nombre Total de Fichiers</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Renommer</translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation>Actualiser</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Editer</translation>
+    </message>
+    <message>
+        <source>View</source>
+        <translation>Voir</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <source>Import</source>
+        <translation>Importer</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Réinitialiser</translation>
+    </message>
+    <message>
+        <source>Export Results</source>
+        <translation>Exporter les résultats</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Sélectionner</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Ajouter</translation>
+    </message>
+    <message>
+        <source>Filters</source>
+        <translation>Filtres</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Réglages</translation>
+    </message>
+    <message>
+        <source>Collection folder</source>
+        <translation>Dossier de Collection</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Recharger</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Ouvrir</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Langue</translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation>Données</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation>Thème</translation>
+    </message>
+    <message>
+        <source>Katalog&apos;s Wiki</source>
+        <translation>Wiki de Katalog</translation>
+    </message>
+    <message>
+        <source>Release Notes</source>
+        <translation>Notes de Version</translation>
+    </message>
+    <message>
+        <source>(requires to restart)</source>
+        <translation>(nécessaire de redémarrer)</translation>
+    </message>
+    <message>
+        <source>Use your system locale</source>
+        <translation>Utiliser la langue du système</translation>
+    </message>
+    <message>
+        <source>Save records for Statistics</source>
+        <translation>Sauver des valeurs pour les Statistiques</translation>
+    </message>
+    <message>
+        <source>Keep 1 backup per catalog</source>
+        <translation>Garder une sauvegarde par catalogue</translation>
+    </message>
+    <message>
+        <source>Filters &amp; Settings</source>
+        <translation>Filtres &amp; Réglages</translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation>Date</translation>
+    </message>
+    <message>
+        <source>Text</source>
+        <translation>Texte</translation>
+    </message>
+    <message>
+        <source>Files found</source>
+        <translation>Fichiers trouvés</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <source>with</source>
+        <translation>avec</translation>
+    </message>
+    <message>
+        <source>in</source>
+        <translation>dans</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Type</translation>
+    </message>
+    <message>
+        <source>show Folders only</source>
+        <translation>uniquement les Dossiers</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>Chemin</translation>
+    </message>
+    <message>
+        <source>with selected Catalog</source>
+        <translation>avec le Catalogue sélectionné</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Taille</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <source>Add Storage</source>
+        <translation>Ajouter un Stockage</translation>
+    </message>
+    <message>
+        <source>Generate</source>
+        <translation>Générer</translation>
+    </message>
+    <message>
+        <source>Create Catalog</source>
+        <translation>Créer le Catalogue</translation>
+    </message>
+    <message>
+        <source>Search storage</source>
+        <translation>Chercher le stockage</translation>
+    </message>
+    <message>
+        <source>Search location</source>
+        <translation>Chercher le lieu</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation>Nouveau</translation>
+    </message>
+    <message>
+        <source>Type of data</source>
+        <translation>Type de données</translation>
+    </message>
+    <message>
+        <source>Edit records</source>
+        <translation>Editer les enregistrements</translation>
+    </message>
+    <message>
+        <source>Number of files</source>
+        <translation>nombre de fichiers</translation>
+    </message>
+    <message>
+        <source>Select Source path</source>
+        <translation>Sélectionner le chemin source</translation>
+    </message>
+    <message>
+        <source>Select Content options</source>
+        <translation>Sélectionner les options de contenu</translation>
+    </message>
+    <message>
+        <source>Define &amp; Create catalog</source>
+        <translation>Définir et créer le catalogue</translation>
+    </message>
+    <message>
+        <source>include File Type</source>
+        <translation>inclure le type de fichier</translation>
+    </message>
+    <message>
+        <source>Other options</source>
+        <translation>Autres options</translation>
+    </message>
+    <message>
+        <source>include Hidden files</source>
+        <translation>inclure les fichiers Cachés</translation>
+    </message>
+    <message>
+        <source>with selected</source>
+        <translation>avec le sélectionné</translation>
+    </message>
+    <message>
+        <source>Devices</source>
+        <translation>Périphériques</translation>
+    </message>
+    <message>
+        <source>Total Space</source>
+        <translation>Espace Total</translation>
+    </message>
+    <message>
+        <source>Used Space</source>
+        <translation>Espace Utilisé</translation>
+    </message>
+    <message>
+        <source>Free Space</source>
+        <translation>Espace libre</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Enregistrer</translation>
+    </message>
+    <message>
+        <source>All</source>
+        <translation>Tous</translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation>Vidéo</translation>
+    </message>
+    <message>
+        <source>All Words</source>
+        <translation>Tous les Mots</translation>
+    </message>
+    <message>
+        <source>Exact Phrase</source>
+        <translation>Phrase Extacte</translation>
+    </message>
+    <message>
+        <source>Begins With</source>
+        <translation>Commence Par</translation>
+    </message>
+    <message>
+        <source>Any Word</source>
+        <translation>Un des Mots</translation>
+    </message>
+    <message>
+        <source>File names or Folder paths</source>
+        <translation>Noms de Fichers ou Chemin des Dossiers</translation>
+    </message>
+    <message>
+        <source>File names only</source>
+        <translation>Noms de Fichers uniquement</translation>
+    </message>
+    <message>
+        <source>Folder path only</source>
+        <translation>Chemins des Dossiers uniquement</translation>
     </message>
 </context>
 </TS>
