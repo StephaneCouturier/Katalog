72b4313634256934ea78582601685326ca375989
StephaneCouturier <stephanecout@yahoo.fr>
2021-10-04 12:18:27 +0200

    https://github.com/StephaneCouturier/Katalog/issues/167

--- mainwindow_tab_settings.cpp
+++ mainwindow_tab_settings.cpp
@@ -40,137 +40,219 @@
 
 //Tab: ALL -----------------------------------------------------------------------------
 
-void MainWindow::on_tabWidget_currentChanged(int index)
-{
-    selectedTab = index;
-    QSettings settings(settingsFilePath, QSettings:: IniFormat);
-    settings.setValue("Settings/selectedTab", ui->tabWidget->currentIndex());
-}
-
-void MainWindow::on_Global_tabWidget_currentChanged(int index)
-{
-    int selectedTabGlobal = index;
-    QSettings settings(settingsFilePath, QSettings:: IniFormat);
-    settings.setValue("Settings/selectedTabGlobal", selectedTabGlobal);
-}
-
-void MainWindow::on_Global_pushButton_ShowHideGlobal_clicked()
-{
-    QString visible = ui->Global_pushButton_ShowHideGlobal->text();
-
-    if ( visible == "<<"){ //Hide
-            ui->Global_pushButton_ShowHideGlobal->setText(">>");
-            ui->Global_tabWidget->setHidden(true);
-            ui->Global_label_Global->setHidden(true);
-
-            QSettings settings(settingsFilePath, QSettings:: IniFormat);
-            settings.setValue("Settings/ShowHideGlobal", ui->Global_pushButton_ShowHideGlobal->text());
-    }
-    else{ //Show
-            ui->Global_pushButton_ShowHideGlobal->setText("<<");
-            ui->Global_tabWidget->setHidden(false);
-            ui->Global_label_Global->setHidden(false);
-
-            QSettings settings(settingsFilePath, QSettings:: IniFormat);
-            settings.setValue("Settings/ShowHideGlobal", ui->Global_pushButton_ShowHideGlobal->text());
+    void MainWindow::on_tabWidget_currentChanged(int index)
+    {
+        selectedTab = index;
+        QSettings settings(settingsFilePath, QSettings:: IniFormat);
+        settings.setValue("Settings/selectedTab", ui->tabWidget->currentIndex());
     }
 
-}
+    void MainWindow::on_Global_tabWidget_currentChanged(int index)
+    {
+        int selectedTabGlobal = index;
+        QSettings settings(settingsFilePath, QSettings:: IniFormat);
+        settings.setValue("Settings/selectedTabGlobal", selectedTabGlobal);
+    }
+
+    void MainWindow::on_Global_pushButton_ShowHideGlobal_clicked()
+    {
+        QString visible = ui->Global_pushButton_ShowHideGlobal->text();
+
+        if ( visible == "<<"){ //Hide
+                ui->Global_pushButton_ShowHideGlobal->setText(">>");
+                ui->Global_tabWidget->setHidden(true);
+                ui->Global_label_Global->setHidden(true);
+
+                QSettings settings(settingsFilePath, QSettings:: IniFormat);
+                settings.setValue("Settings/ShowHideGlobal", ui->Global_pushButton_ShowHideGlobal->text());
+        }
+        else{ //Show
+                ui->Global_pushButton_ShowHideGlobal->setText("<<");
+                ui->Global_tabWidget->setHidden(false);
+                ui->Global_label_Global->setHidden(false);
+
+                QSettings settings(settingsFilePath, QSettings:: IniFormat);
+                settings.setValue("Settings/ShowHideGlobal", ui->Global_pushButton_ShowHideGlobal->text());
+        }
+
+    }
 
 //Tab: FILTERS -----------------------------------------------------------------------------
-void MainWindow::on_Filters_pushButton_ResetGlobal_clicked()
-{
-        ui->Filters_comboBox_SelectLocation->setCurrentText(tr("All"));
-        ui->Filters_comboBox_SelectStorage->setCurrentText(tr("All"));
-        ui->Filters_comboBox_SelectCatalog->setCurrentText(tr("All"));
-}
-//----------------------------------------------------------------------
-void MainWindow::on_Filters_comboBox_SelectLocation_currentIndexChanged(const QString &selectedLocation)
-{
-    //save selection in settings file;
-    QSettings settings(settingsFilePath, QSettings:: IniFormat);
-    settings.setValue("LastSearch/SelectedSearchLocation", selectedLocation);
+    void MainWindow::on_Filters_pushButton_ResetGlobal_clicked()
+    {
+            ui->Filters_comboBox_SelectLocation->setCurrentText(tr("All"));
+            ui->Filters_comboBox_SelectStorage->setCurrentText(tr("All"));
+            ui->Filters_comboBox_SelectCatalog->setCurrentText(tr("All"));
+    }
+    //----------------------------------------------------------------------
+    void MainWindow::on_Filters_comboBox_SelectLocation_currentIndexChanged(const QString &selectedLocation)
+    {
+        //save selection in settings file;
+        QSettings settings(settingsFilePath, QSettings:: IniFormat);
+        settings.setValue("LastSearch/SelectedSearchLocation", selectedLocation);
 
-    //Load matching Storage
-    refreshStorageSelectionList(selectedLocation);
+        //Load matching Storage
+        refreshStorageSelectionList(selectedLocation);
 
-    selectedSearchLocation = selectedLocation;
+        selectedSearchLocation = selectedLocation;
 
-    //Load matching Catalog
-    loadCatalogsToModel();
-    loadStorageTableToModel();
-    refreshStorageStatistics();
+        //Load matching Catalog
+        loadCatalogsToModel();
+        loadStorageTableToModel();
+        refreshStorageStatistics();
 
-}
-//----------------------------------------------------------------------
-void MainWindow::on_Filters_comboBox_SelectStorage_currentIndexChanged(const QString &selectedStorage)
-{
-    //save selection in settings file;
-    QSettings settings(settingsFilePath, QSettings:: IniFormat);
-    settings.setValue("LastSearch/SelectedSearchStorage", selectedStorage);
+    }
+    //----------------------------------------------------------------------
+    void MainWindow::on_Filters_comboBox_SelectStorage_currentIndexChanged(const QString &selectedStorage)
+    {
+        //save selection in settings file;
+        QSettings settings(settingsFilePath, QSettings:: IniFormat);
+        settings.setValue("LastSearch/SelectedSearchStorage", selectedStorage);
 
-    //Get selected Location and Storage
-    QString selectedLocation = ui->Filters_comboBox_SelectLocation->currentText();
-    //QString selectedStorage  = ui->Filters_comboBox_SelectStorage->currentText();
+        //Get selected Location and Storage
+        QString selectedLocation = ui->Filters_comboBox_SelectLocation->currentText();
+        //QString selectedStorage  = ui->Filters_comboBox_SelectStorage->currentText();
 
-    //Load matching Storage
-    refreshCatalogSelectionList(selectedLocation, selectedStorage);
+        //Load matching Storage
+        refreshCatalogSelectionList(selectedLocation, selectedStorage);
 
-    selectedSearchStorage = selectedStorage;
+        selectedSearchStorage = selectedStorage;
 
-    //Load matching Catalog
-    loadCatalogsToModel();
-}
-//----------------------------------------------------------------------
-void MainWindow::on_Filters_comboBox_SelectCatalog_currentIndexChanged(const QString &selectedCatalog)
-{
-    //save selection in settings file;
-    QSettings settings(settingsFilePath, QSettings:: IniFormat);
-    settings.setValue("LastSearch/SelectedSearchCatalog", selectedCatalog);
+        //Load matching Catalog
+        loadCatalogsToModel();
+    }
+    //----------------------------------------------------------------------
+    void MainWindow::on_Filters_comboBox_SelectCatalog_currentIndexChanged(const QString &selectedCatalog)
+    {
+        //save selection in settings file;
+        QSettings settings(settingsFilePath, QSettings:: IniFormat);
+        settings.setValue("LastSearch/SelectedSearchCatalog", selectedCatalog);
 
-}
+    }
 
 //Tab: SETTINGS -----------------------------------------------------------------------------
 
-void MainWindow::on_Settings_checkBox_SaveRecordWhenUpdate_stateChanged()
-{
-    QSettings settings(settingsFilePath, QSettings:: IniFormat);
-    settings.setValue("Settings/AutoSaveRecordWhenUpdate", ui->Settings_checkBox_SaveRecordWhenUpdate->isChecked());
-}
+//SETTINGS / Collection ---------------------------------------------------------------------
 
-void MainWindow::on_Settings_checkBox_KeepOneBackUp_stateChanged()
-{
-    QSettings settings(settingsFilePath, QSettings:: IniFormat);
-    settings.setValue("Settings/KeepOneBackUp", ui->Settings_checkBox_KeepOneBackUp->isChecked());
-}
+    void MainWindow::on_Collection_pushButton_SelectFolder_clicked()
+    {
+        //Open a dialog for the user to select the directory of the collection where catalog files are stored.
+        QString dir = QFileDialog::getExistingDirectory(this, tr("Select the directory for this collection"),
+                                                        collectionFolder,
+                                                        QFileDialog::ShowDirsOnly
+                                                        | QFileDialog::DontResolveSymlinks);
 
-void MainWindow::on_Settings_comboBox_Theme_currentIndexChanged(int index)
-{
-    QSettings settings(settingsFilePath, QSettings:: IniFormat);
-    settings.setValue("Settings/Theme", index);
-}
+        //Unless the selection was cancelled, set the new collection folder, and refresh the list of catalogs
+        if ( dir !=""){
 
-void MainWindow::on_Settings_checkBox_CheckVersion_stateChanged()
-{
-    QSettings settings(settingsFilePath, QSettings:: IniFormat);
-    settings.setValue("Settings/CheckVersion", ui->Settings_checkBox_CheckVersion->isChecked());
-}
+            collectionFolder = dir;
+
+            //set the new path in Colletion tab
+            ui->Collection_lineEdit_CollectionFolder->setText(collectionFolder);
+
+            //redefine the path of the Storage file
+            storageFilePath = collectionFolder + "/" + "storage.csv";
+
+            //save Settings for the new collection folder value;
+            saveSettings();
+
+            //load the collection for this new folder;
+            loadCollection();
+
+        }
+
+        //Reset selected catalog values (to avoid actions on the last selected one)
+        selectedCatalogFile="";
+        selectedCatalogName="";
+        selectedCatalogPath="";
+    }
+    //----------------------------------------------------------------------
+    void MainWindow::on_Collection_lineEdit_CollectionFolder_returnPressed()
+    {
+        loadCollection();
+    }
+    //----------------------------------------------------------------------
+    void MainWindow::on_Collection_pushButton_OpenFolder_clicked()
+    {
+        //Open the selected collection folder
+        QDesktopServices::openUrl(QUrl::fromLocalFile(collectionFolder));
+    }
+    //----------------------------------------------------------------------
+    void MainWindow::on_Collection_pushButton_Reload_clicked()
+    {
+        loadCollection();
+    }
+
+    //----------------------------------------------------------------------
+    void MainWindow::loadCollection()
+    {
+        //Load Storage list and refresh their statistics
+            loadStorageFileToTable();
+            loadStorageTableToModel();
+            refreshStorageStatistics();
+
+       //load Catalog list, Location list, Storage list, Statistics
+            loadCatalogFilesToTable();
+
+            loadCatalogsToModel();
+
+            refreshLocationSelectionList();
+            refreshStorageSelectionList(tr("All"));
+            refreshCatalogSelectionList(tr("All"), tr("All"));
+
+            //restore Search catalog selection
+            //ui->Filters_comboBox_SelectLocation->setCurrentText(selectedSearchLocation);
+            //ui->Filters_comboBox_SelectStorage->setCurrentText(selectedSearchStorage);
+            //ui->Filters_comboBox_SelectCatalog->setCurrentText(selectedSearchCatalog);
+
+            statisticsFilePath = collectionFolder + "/" + "statistics.csv";
+            loadStatisticsData();
+            loadStatisticsChart();
+
+            //hide buttons to force user to select a catalog before allowing any action.
+            hideCatalogButtons();
+    }
+
+//SETTINGS / Settings ---------------------------------------------------------------------
+
+    void MainWindow::on_Settings_checkBox_SaveRecordWhenUpdate_stateChanged()
+    {
+        QSettings settings(settingsFilePath, QSettings:: IniFormat);
+        settings.setValue("Settings/AutoSaveRecordWhenUpdate", ui->Settings_checkBox_SaveRecordWhenUpdate->isChecked());
+    }
+
+    void MainWindow::on_Settings_checkBox_KeepOneBackUp_stateChanged()
+    {
+        QSettings settings(settingsFilePath, QSettings:: IniFormat);
+        settings.setValue("Settings/KeepOneBackUp", ui->Settings_checkBox_KeepOneBackUp->isChecked());
+    }
+
+    void MainWindow::on_Settings_comboBox_Theme_currentIndexChanged(int index)
+    {
+        QSettings settings(settingsFilePath, QSettings:: IniFormat);
+        settings.setValue("Settings/Theme", index);
+    }
+
+    void MainWindow::on_Settings_checkBox_CheckVersion_stateChanged()
+    {
+        QSettings settings(settingsFilePath, QSettings:: IniFormat);
+        settings.setValue("Settings/CheckVersion", ui->Settings_checkBox_CheckVersion->isChecked());
+    }
 
 //SETTINGS / About ---------------------------------------------------------------------
 
-void MainWindow::on_Settings_pushButton_Wiki_clicked()
-{
-    QDesktopServices::openUrl(QUrl("https://github.com/StephaneCouturier/Katalog/wiki"));
-}
+    void MainWindow::on_Settings_pushButton_Wiki_clicked()
+    {
+        QDesktopServices::openUrl(QUrl("https://github.com/StephaneCouturier/Katalog/wiki"));
+    }
 
-void MainWindow::on_Settings_pushButton_ReleaseNotes_clicked()
-{
-    QDesktopServices::openUrl(QUrl("https://github.com/StephaneCouturier/Katalog/releases"));
-}
+    void MainWindow::on_Settings_pushButton_ReleaseNotes_clicked()
+    {
+        QDesktopServices::openUrl(QUrl("https://github.com/StephaneCouturier/Katalog/releases"));
+    }
 
-void MainWindow::on_Settings_comboBox_Language_currentTextChanged(const QString &selectedLanguage)
-{
-    QSettings settings(settingsFilePath, QSettings:: IniFormat);
-    settings.setValue("Settings/Language", selectedLanguage);
-
-}
+    void MainWindow::on_Settings_comboBox_Language_currentTextChanged(const QString &selectedLanguage)
+    {
+        QSettings settings(settingsFilePath, QSettings:: IniFormat);
+        settings.setValue("Settings/Language", selectedLanguage);
+    }
