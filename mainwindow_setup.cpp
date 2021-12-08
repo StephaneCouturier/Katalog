0aa4870a6993a87982ec0dbfc8474a62ef60a651
Stéphane Couturier <contact@symbioxy.com>
2021-12-09 00:24:25 +0100

    https://github.com/StephaneCouturier/Katalog/issues/214

--- mainwindow_setup.cpp
+++ mainwindow_setup.cpp
@@ -263,7 +263,6 @@
     void MainWindow::hideDevelopmentUIItems()
     {
         //Search
-            ui->Search_pushButton_FileFoundMoreStatistics->hide();
         //Catalogs
 
         //Create
