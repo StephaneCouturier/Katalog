a767d3150b872a7ca06ee33dda7d4da8174992cd
Stéphane Couturier <katalog@stephanecouturier.com>
2023-02-27 18:36:18 +0100

    https://github.com/StephaneCouturier/Katalog/issues/411

--- mainwindow_tab_catalogs.cpp
+++ mainwindow_tab_catalogs.cpp
@@ -353,6 +353,7 @@
 
                 loadCatalogsTableToModel();
                 refreshCatalogSelectionList("","");
+                loadStorageTableToSelectionTreeModel();
             }
         }
         //----------------------------------------------------------------------
