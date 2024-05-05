0a4239fd91e6b56320779d698cf66b3271a43702
Stéphane Couturier <katalog@stephanecouturier.com>
2024-05-05 11:29:46 +0200

    https://github.com/StephaneCouturier/Katalog/issues/505

---
+++ docs/assets/js/2398e09f.05ab0979.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkatalog_pages_2=self.webpackChunkkatalog_pages_2||[]).push([[659],{287:a=>{a.exports=JSON.parse('{"tags":[{"label":"release","permalink":"/Katalog/blog/tags/release","count":1}]}')}}]);
---
+++ docs/fr/assets/js/d15fb396.141af3f0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkatalog_pages_2=self.webpackChunkkatalog_pages_2||[]).push([[111],{4833:a=>{a.exports=JSON.parse('{"tags":[{"label":"release","permalink":"/Katalog/fr/blog/tags/release","count":1}]}')}}]);
