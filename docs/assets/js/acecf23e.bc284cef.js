156f939be43584cf84c3fd1476e478947d8e6eb6
Stéphane Couturier <katalog@stephanecouturier.com>
2024-05-07 17:58:06 +0200

    https://github.com/StephaneCouturier/Katalog/issues/505

--- docs/assets/js/acecf23e.bc284cef.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkatalog_pages_2=self.webpackChunkkatalog_pages_2||[]).push([[1903],{1912:a=>{a.exports=JSON.parse('{"blogBasePath":"/home/stephane/Documents/Informatique/Katalog/Documentation/Katalog-pages/build/blog","blogTitle":"Blog"}')}}]);
